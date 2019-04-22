9f620a538e527652dbd8e35c19e6faf3c020845f
Ninad Kanthi <ninad_kanthi@hotmail.com>
2019-04-22 15:39:30 +0100

    Adding Account sign-in sign-out functionality

---
+++ MVCSecureApp/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using MVCSecureApp.Utils;
+
+namespace MVCSecureApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUrl, AllowRefresh = true },
+                OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+            // Remove all cache entries for this user and send an OpenID Connect sign-out request.
+            string userObjectID = User.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+            var authContext = new Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext(AzureAdOptions.Settings.Authority,
+                                                        new NaiveSessionCache(userObjectID, HttpContext.Session));
+            authContext.TokenCache.Clear();
+
+            // Let Azure AD sign-out
+            var callbackUrl = Url.Action(nameof(SignedOut), "Account", values: null, protocol: Request.Scheme);
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl, AllowRefresh = true },
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult SignedOut()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
